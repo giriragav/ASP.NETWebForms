8ebaf0309fb46904ec8a0bed7038f979d327a9df
venkat-giri <giri.ragav@gmail.com>
2017-06-10 18:07:54 -0500

    Added data control to display top navigation data

--- WingtipToys/WingtipToys/Site.Master.cs
+++ WingtipToys/WingtipToys/Site.Master.cs
@@ -7,6 +7,8 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -69,7 +71,13 @@ namespace WingtipToys
 
         protected void Page_Load(object sender, EventArgs e)
         {
+        
+        }
+        public IQueryable<Category> GetCategories()
+        {
+            var db = new WingtipToys.Models.ProductContext();
 
+            return db.Categories;
         }
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
