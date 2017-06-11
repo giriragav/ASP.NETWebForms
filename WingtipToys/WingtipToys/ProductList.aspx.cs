16247766802e100f47fa4b38200ff17824f3589d
venkat-giri <giri.ragav@gmail.com>
2017-06-10 19:31:04 -0500

    Rendedred product detail with list view

--- WingtipToys/WingtipToys/ProductList.aspx.cs
+++ WingtipToys/WingtipToys/ProductList.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WingtipToys.Models;
 
 namespace WingtipToys
 {
@@ -12,6 +14,13 @@ namespace WingtipToys
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? productID)
+        {
+            var db = new ProductContext();
+
+            return db.Products.Where(p => p.CategoryID == productID);
         }
     }
 }
